f865c43b52d9426eea2fcd32aa7fab9dd528d194
DELL <DELL@LUCIFER-BB1UOJ8>
2019-03-17 11:15:30 +0630

    master

--- src/main/java/com/example/springHelloworldTest/controller/HomeController.java
+++ src/main/java/com/example/springHelloworldTest/controller/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HomeController {
-    @GetMapping("/welcom")
+    @GetMapping("/")
     public String welcome(){
         return "Hello Spring Demo";
     }
